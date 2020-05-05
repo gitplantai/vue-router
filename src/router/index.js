8be2b93ac6a98965a16eb308cec8b27478de2727
gitflowermany <Aida.Ai.slack@gmail.com>
2020-05-06 06:42:54 +0900

    routerのindex.js  作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Practice from '../views/Practice.vue'
+import Privacy from '../views/Privacy.vue'
+import Router from '../views/Router.vue'
+import User from '../views/User.vue'
+import FirstChild from '../views/user/FirstChild.vue'
+import SecondChild from '../views/user/SecondChild.vue'
+import LastChild from '../views/user/LastChild.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +18,53 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/practice',
+    name: 'practice',
+    component: Practice
+  },
+  {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/privacy',
+    name: 'privacy',
+    component: Privacy
+  },
+  {
+    path: '/router',
+    name: 'router',
+    component: Router
+  },
+  {
+    path: '/user/:id',
+    name: 'user',
+    component: User,
+    children: [
+      {
+        path: 'first',    //スラッシュはいらない！！！
+        name: 'first',
+        component: FirstChild
+      },
+      {
+        path: 'second',
+        name: 'second',
+        component: SecondChild
+      },
+      {
+        path: 'last',
+        name: 'last',
+        component: LastChild
+      },
+    ],
+  },
+  {
+    path: '*',      // ワイルドカードは最後に入れること！
+    redirect: '/',  // top pageにリダイレクト
   }
 ]
 
